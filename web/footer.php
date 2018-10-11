90c6563bc32bd6070319da9a46ac4daa43ee7a2f
MaximeF <fumelmax@gmail.com>
2018-10-11 07:44:02 +0200

    footer

--- web/footer.php
+++ web/footer.php
@@ -7,22 +7,25 @@
 					<p>© 2016 Modern Shoppe . All rights reserved | Design by <a href="http://w3layouts.com" target="_blank">W3layouts</a></p>
 				</div>
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".7s">
-					<h3>Popular</h3>
+					<h3>Useful links</h3>
 					<ul>
 						<li><a href="about.php">About Us</a></li>
-						<li><a href="products.php">new</a></li>
 						<li><a href="contact.php">Contact Us</a></li>
 						<li><a href="faq.php">FAQ</a></li>
-						<li><a href="checkout.php">Wishlist</a></li>
+						<li><a href="#">Legal notice</a></li>
 					</ul>
 				</div>
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".9s">
-					<h3>Subscribe</h3>
-					<p>Sign Up Now For More Information <br> About Our Company </p>
-					<form>
-						<input type="text" placeholder="Enter Your Email" required="">
-						<input type="submit" value="Go">
-					</form>
+					<h3>Projet S3</h3>
+					<p>Réalisé par les étudiants de l'ENSIBS en 2ème année de la formation Informatique de Confiance
+						<ul>
+							<li> Othmane Dziri </li>
+							<li> Maxime Mathis--Fumel </li>
+							<li> Robin Moalic </li>
+							<li> Yassin Ourkia </li>
+						</ul>
+					</p>
+					
 				</div>
 				<div class="clearfix"></div>
 			</div>
