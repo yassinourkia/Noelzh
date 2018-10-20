4b033ef358e1e1dcefe4f071f3d4c7fa85042b07
Robin MOALIC <robin.moalic@yahoo.fr>
2018-10-20 10:25:57 +0200

    Trad: header + footer

--- web/footer.php
+++ web/footer.php
@@ -3,16 +3,16 @@
 		<div class="container">
 			<div class="footer-info">
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".5s">
-					<h4 class="footer-logo"><a href="index.php">Noelzh <span class="tag">Everything for Christmas </span> </a></h4>
-					<p>© 2016 Modern Shoppe . All rights reserved | Design by <a href="http://w3layouts.com" target="_blank">W3layouts</a></p>
+					<h4 class="footer-logo"><a href="index.php">Noelzh <span class="tag">Tout ce qu'il vous faut pour un noël réussi </span> </a></h4>
+					<p>© 2016 Modern Shoppe. Tous droits réservés | Designer: <a href="http://w3layouts.com" target="_blank">W3layouts</a></p>
 				</div>
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".7s">
-					<h3>Useful links</h3>
+					<h3>Liens utiles</h3>
 					<ul>
-						<li><a href="about.php">About Us</a></li>
-						<li><a href="contact.php">Contact Us</a></li>
+						<li><a href="about.php">À propos</a></li>
+						<li><a href="contact.php">Contacter nous</a></li>
 						<li><a href="faq.php">FAQ</a></li>
-						<li><a href="#">Legal notice</a></li>
+						<li><a href="#">Mentions légales</a></li>
 					</ul>
 				</div>
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".9s">
