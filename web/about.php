52f17ab34b884e9dd3503ac773049b1a70118ad7
Robin MOALIC <robin.moalic@yahoo.fr>
2018-10-20 11:20:47 +0200

    trad: ssssssss

--- web/about.php
+++ web/about.php
@@ -6,7 +6,7 @@ require_once('header.php');
 		<div class="container">
 			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
 				<li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Accueil</a></li>
-				<li class="active">À propot</li>
+				<li class="active">À propos</li>
 			</ol>
 		</div>
 	</div>
@@ -15,7 +15,7 @@ require_once('header.php');
 	<div class="about">
 		<div class="container">
 			<div class="title-info wow fadeInUp animated" data-wow-delay=".5s">
-				<h3 class="title">À<span> propot</span></h3>
+				<h3 class="title">À<span> propos</span></h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur </p>
 			</div>
 			<div class="about-info">
