e1e6d6ca8cf033ce2b83da907f6eedf9467e516c
OURKIA YASSIN <ourkia@MacBook-Pro-de-OURKIA.local>
2018-10-25 10:19:36 +0200

    Comment scripts

---
+++ Comment/commentlist.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+ * Include our MySQL connection.
+ */
+	require '../connect.php';
+	$stmt = $connect->query("SELECT r.text, u.name FROM ratings r, users u where u.id = r.id_users ");
+	echo '<div class="panel-body"><table class="table table-hover table-responsive" ';
+	while ($row = $stmt->fetch()) 
+	{ 
+		echo '<tr class="active"> <td>Nom </td><td> '.$row["name"].'</td></tr><tr><td>Commentaire </td><td> '.$row["text"].'</td></tr>';
+	}
+	echo '</table></div>' ;
+?>
---
+++ Comment/postcomment.php
@@ -0,0 +1,61 @@
+<?php
+//postcomment.php
+
+/**
+ * Start the session.
+ */
+session_start();
+
+
+
+/**
+ * Include our MySQL connection.
+ */
+require '../connect.php';
+if(! isset($_SESSION['user_id']))
+{
+    header("location:../web/login.php");
+}
+else
+{
+	if(isset($_POST["send"]))
+	{
+		echo "here";
+		if( isset($_POST["contenu"]) && isset($_SESSION['id_products']) && isset($_SESSION['user_id']) )
+	    {
+    		
+    		$insert_query = "
+				INSERT INTO `ratings` (`id`, `text`, `rating`, `id_products`, `id_users`) VALUES (NULL, :contenu, :rating, :id_products,:id_users)
+				";
+				$statement = $connect->prepare($insert_query);
+				$statement->execute(
+					array(
+						':contenu'			=>	$_POST['contenu'],
+						':rating'			=>	4,
+						':id_products'		=>	$_SESSION['id_products'],
+						':id_users'			=>	$_SESSION['user_id']	
+					)
+				);
+				$result = $statement->fetchAll();
+				
+				if(isset($result))
+				{
+					$message ='Merci Pour votre commentaire';
+					header("location:../web/single.php?pid=".$_SESSION['id_products']."&message=$message");	
+				}
+				
+
+		}
+		else
+		{
+				$message = 'Echec : Commentaire non posté';
+				header("location:../web/single.php?pid=1&message=$message");
+		}		 
+	}
+	else
+	{
+		echo "mochkil";
+	}
+}
+
+?>
