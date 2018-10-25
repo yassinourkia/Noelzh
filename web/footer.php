4fb24b70663e3a5898f057176f3627792e2bc551
Othmane <othmanedziri@gmail.com>
2018-10-25 11:54:45 +0200

    edit

--- web/footer.php
+++ web/footer.php
@@ -4,7 +4,7 @@
 			<div class="footer-info">
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".5s">
 					<h4 class="footer-logo"><a href="index.php">Noelzh <span class="tag">Tout ce qu'il vous faut pour un Noël réussi </span> </a></h4>
-					<p>© 2016 Modern Shoppe. Tous droits réservés | Designer: <a href="http://w3layouts.com" target="_blank">W3layouts</a></p>
+					<p>©2018 Noelzh. Tous droits réservés 
 				</div>
 				<div class="col-md-4 footer-grids wow fadeInUp animated" data-wow-delay=".7s">
 					<h3>Liens utiles</h3>
